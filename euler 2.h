840f7b4fda429eaab9d00a5e0192c965541e49fc
Brian Jones <ba.jones@me.com>
2012-12-06 22:05:09 +1000

    change of directory structure

---
+++ euler 2.h
@@ -0,0 +1,13 @@
+//
+//  euler 2.h
+//  Euler Problems
+//
+//  Created by Brian on 5/12/12.
+//  Copyright (c) 2012 Brian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface euler_2 : NSObject
+
+@end
---
+++ main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Euler Problems
+//
+//  Created by Brian on 5/12/12.
+//  Copyright (c) 2012 Brian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
