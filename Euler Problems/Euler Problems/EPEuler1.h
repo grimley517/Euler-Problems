d33aeb3a4d8d96d774370f02a1184d7995f0674b
Brian Jones <ba.jones@me.com>
2012-12-13 13:40:46 +1000

    Added view, and template class for problem 1

---
+++ Euler Problems/Euler Problems/EPEuler1.h
@@ -0,0 +1,13 @@
+//
+//  EPEuler1.h
+//  Euler problems
+//
+//  Created by Brian on 13/12/12.
+//  Copyright (c) 2012 Brian Jones. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EPEuler1 : NSObject
+
+@end
---
+++ Euler Problems/Euler Problems/EPEuler1.m
@@ -0,0 +1,13 @@
+//
+//  EPEuler1.m
+//  Euler problems
+//
+//  Created by Brian on 13/12/12.
+//  Copyright (c) 2012 Brian Jones. All rights reserved.
+//
+
+#import "EPEuler1.h"
+
+@implementation EPEuler1
+
+@end
